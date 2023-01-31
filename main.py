f3f8c343152675d41bdab775de65ea666cc1d5e2
Oleg Novosad <adminnovik@gmail.com>
2023-01-31 10:47:27 +0200

    Added second player.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import random
 
-value_1 = random.randint(1, 6)
-value_2 = random.randint(1, 6)
+p1_value_1 = random.randint(1, 6)
+p1_value_2 = random.randint(1, 6)
 
-print("You got:", value_1, value_2)
+p2_value_1 = random.randint(1, 6)
+p2_value_2 = random.randint(1, 6)
+
+p1_sum = p1_value_1 + p1_value_2
+p2_sum = p2_value_1 + p2_value_2
+
+if p1_sum > p2_sum:
+    print("Player 1 won")
+elif p1_sum < p2_sum:
+    print("Player 2 won")
+else:
+    print("AMAZING! It is DRAW!")
+    
